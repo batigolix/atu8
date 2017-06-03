c7aeafccfa9bfa4a98ef75738da923515e01e049
Boris Doesborg <bdoesborg@gmail.com>
2017-06-03 23:05:10 +0200

    theming

---
+++ web/modules/custom/atu8_tools/src/Plugin/Block/SliderBlock.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\atu8_tools\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\image\Entity\ImageStyle;
+
+/**
+ * Provides a 'SliderBlock' block.
+ *
+ * @Block(
+ *  id = "slider_block",
+ *  admin_label = @Translation("Slider block"),
+ * )
+ */
+class SliderBlock extends BlockBase {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build() {
+        $build = [];
+        $query = \Drupal::entityQuery('node');
+        $query->condition('type', 'photo');
+        $query->sort('nid', 'ASC');
+        $nids = $query->execute();
+        $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($nids);
+        $build['slides']['#prefix'] = '<ul id="lightSlider">';
+        $build['slides']['#suffix'] = '</ul>';
+        foreach ($nodes as $node) {
+            $file = $node->field_image->entity;
+            if ($file) {
+                $uri = $file->getFileUri();
+                $large_uri = ImageStyle::load('jumbo')->buildUrl($uri);
+                $thumbnail_uri = ImageStyle::load('thumbnail')->buildUrl($uri);
+                $build['slides'][] = [
+                    '#theme' => 'slider_item',
+                    '#large_uri' => $large_uri,
+                    '#thumbnail_uri' => $thumbnail_uri,
+                ];
+            }
+        }
+        return $build;
+    }
+}
+
---
+++ web/themes/dollard/js/responsive-menu.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Provides responsive menu script.
+ */
+
+// Toggles between adding and removing the "responsive" class to topnav when
+// the user clicks on the icon.
+function openResponsiveMenu() {
+  var x = document.getElementById("responsive-menu");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  }
+  else {
+    x.className = "topnav";
+  }
+}
